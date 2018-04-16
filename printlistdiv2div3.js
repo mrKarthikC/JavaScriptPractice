275089922885bd9b1018f8d0d3a2fc186dbfaf75
mrKarthikC <37879494+mrKarthikC@users.noreply.github.com>
2018-04-16 23:34:58 +0530

    problem solving

---
+++ printlistdiv2div3.js
@@ -0,0 +1,28 @@
+
+
+for (n = 1 ; n <= 100 ; n++)
+
+{
+	
+	if(n%2 == 0 && n%3 == 0)
+	{
+		console.log("Karthik Ch");
+
+	
+	}
+	else if(n%2 == 0)
+	{
+		console.log("Karthik");
+	}
+	else if(n%3 == 0)
+	{
+		console.log("Ch");
+	}
+	else 
+	{ 
+		console.log(n);
+	}
+
+	}
+
+
